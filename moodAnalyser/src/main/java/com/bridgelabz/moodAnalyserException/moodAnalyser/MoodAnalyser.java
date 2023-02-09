45be565fd37de0c64e8ab779f83bdc50a15bfe31
komalarao <manoharvakamullu1999@gmail.com>
2023-02-09 07:02:47 -0800

    added : refactor the mood analyser code

--- moodAnalyser/src/main/java/com/bridgelabz/moodAnalyserException/moodAnalyser/MoodAnalyser.java
+++ moodAnalyser/src/main/java/com/bridgelabz/moodAnalyserException/moodAnalyser/MoodAnalyser.java
@@ -1,10 +1,16 @@
 package com.bridgelabz.moodAnalyserException.moodAnalyser;
 
 public class MoodAnalyser {
+	private String message;
+
+	public void MoodAnalyser() {
+		
+	}
 	public String moodAnalyser(String message) {
+		this.message = message;
 		if (message.contains("Sad"))
 			return "SAD";
 		else
-		return "HAPPY";
+			return "HAPPY";
 	}
 }
